b745a3905ba243fdc43d9f3d7a35e2dfd50215c9
Daemyon <damioncms@gmail.com>
2020-01-21 20:43:57 -0600

    Security patches

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+import { commands, window } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,21 +17,22 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let disposable = commands.registerCommand('extension.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		window.showInformationMessage('Hello VS Code!');
 	});
 
 	context.subscriptions.push(disposable);
 }
-exports.activate = activate;
+const _activate = activate;
+export { _activate as activate };
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
-module.exports = {
+export default {
 	activate,
 	deactivate
 }
