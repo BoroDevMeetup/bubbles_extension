ef1e5de8b41cfcac4d9ea687c31607a323987b70
Daemyon <damioncms@gmail.com>
2020-01-21 22:09:36 -0600

    Starting over using new extension tools

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { commands, window } from 'vscode';
+const vscode = require('vscode');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,27 +12,26 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bubbles-extension" is now active!');
+	console.log('Congratulations, your extension "bubbles-ext" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = commands.registerCommand('extension.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		window.showInformationMessage('Hello VS Code!');
+		vscode.window.showInformationMessage('Hello World!');
 	});
 
 	context.subscriptions.push(disposable);
 }
-const _activate = activate;
-export { _activate as activate };
+exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
-export default {
+module.exports = {
 	activate,
 	deactivate
 }
